083040f7110fb88b85abf6401aa24be6a3c61ec1
John DH. Park <john.dh.park@me.com>
2023-03-02 17:21:44 -0800

    Created the writeCards() and countDown() functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+	const messages = [];
+
+	for (const name of names) {
+		messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+	}
+
+	return messages;
+}
+
+function countDown(start) {
+	for (let num = start; num >= 0; --num) {
+		console.log(num);
+	}
+}
